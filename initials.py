85dc46c631df2f3b3915c5c515bd44b8177086f5
Amewu Benjamin Yaa-nieta <2425402295@live.gctu.edu.gh>
2026-05-31 14:02:15 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
 
-def main():
-  pass #code goes here
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Benjamin Amewu Y.
+# STUDENT ID:  2425402295
+# Email: 2425402295@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Benjamin Amewu Y., this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+
+def main():
+  pass # Code goes here
+    # Prints my initials in large stylized letters
+
+    print()
+    print("...BAY")
+    print()
+    print("...BBBBBBBBB................AAA............YYY......YYY.....")
+    print("...BB......BBB.............AA.AA............YY......YY......")
+    print("...BB.......BB............AA...AA............Y......Y.......")
+    print("...BB.......BB...........AA.....AA............Y....Y........")
+    print("...BBBBBBBBB............AAAAAAAAAAA............YYYY.........")
+    print("...BB.......BB..........AA.......AA.............YYY.........")
+    print("...BB........BB.........AA.......AA..............Y..........")
+    print("...BB........BB.........AA.......AA..............Y..........")
+    print("...BB........BB...**....AA.......AA..............Y........**")
+    print("...BBBBBBBBBB.....**....AA.......AA...**.........Y........**")
+
+    print()
 
 
 main()
